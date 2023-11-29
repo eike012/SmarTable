cdeed90defb3ec9141d57ff0b632b5e988d93445
eike012 <eike_souza@hotmail.com>
2023-11-29 15:18:03 -0300

    Check if items in JSON are equal to reference JSON

---
+++ dishCheck.py
@@ -0,0 +1,21 @@
+def dishCheck(JSONArray, referenceArray):
+    count = 0
+    if len(JSONArray[0][0] == 3):
+        for i in range(len(JSONArray)):
+            cmpTitle = JSONArray[i][0]['Title']
+            cmpPrice = JSONArray[i][0]['Price 1']
+            for j in range(size(referenceArray)):
+                if cmpTitle == referenceArray[j][0]['Title'] and cmpPrice == referenceArray[j][0]['Price 1']:
+                    count += 1
+    else:
+        for i in range(len(JSONArray)):
+            cmpTitle = JSONArray[i][0]['Title']
+            cmpPrice1 = JSONArray[i][0]['Price 1']
+            cmpPrice2 = JSONArray[i][0]['Price 2']
+            for j in range(len(referenceArray)):
+                if cmpTitle == referenceArray[j][0]['Title'] and cmpPrice == referenceArray[j][0]['Price 1'] and cmpPrice2 == referenceArray[j][0]['Price 2']:
+                    count += 1
+    
+    return count/len(JSONArray)
+
+
